bb2b153fdf08af4b8c3cb11698b015b87537ab5d
Abel N. Nieva <abelnnieva@gmail.com>
2025-09-24 21:25:48 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+const Pizza = (props) => {
+  return React.createElement("div", null, [
+    React.createElement("h1", null, props.name),
+    React.createElement("p", null, props.description),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", null, [
+    React.createElement("h1", null, "Padre Gino’s"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Mozzarella Cheese, Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Margherita Pizza",
+      description: "Fresh Mozzarella, Basil",
+    }),
+    React.createElement(Pizza, {
+      name: "The BBQ Chicken Pizza",
+      description: "BBQ Sauce, Chicken, Red Onions",
+    }),
+    React.createElement(Pizza, {
+      name: "The Veggie Pizza",
+      description: "Bell Peppers, Onions, Mushrooms",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
